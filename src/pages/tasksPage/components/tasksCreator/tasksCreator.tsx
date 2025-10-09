4badced179e02eb27d0b2830746ebd64d9127b50
Colin Maher <colin.maher@proton.me>
2025-10-09 11:17:24 +0200

    feat: improvements to tasksPage

---
+++ src/pages/tasksPage/components/tasksCreator/tasksCreator.tsx
@@ -0,0 +1,68 @@
+import * as React from "react";
+
+import { InputText } from "components/inputText/inputText";
+import { InputDate } from "components/inputDate/inputDate";
+import { Button } from "components/button/button";
+import { mergeReducer } from "utils/mergeReducer";
+import { DatabaseRecord } from "types/general";
+import { Task } from "types/task";
+import { createRecord } from "services/database/createRecord";
+
+import styles from "./styles.module.scss";
+
+export const TasksCreator = () => {
+  const [newTask, updateNewTask] = React.useReducer(
+    mergeReducer<Omit<Task, keyof DatabaseRecord>>,
+    {
+      dueDate: Date.now(),
+      name: "",
+    }
+  );
+
+  const [showDueDate, setShowDueDate] = React.useState(false);
+
+  const handleAdd = () => {
+    void createRecord({
+      collection: "tasks",
+      data: newTask,
+    });
+  };
+
+  return (
+    <div className={styles.container}>
+      <InputText
+        label="Task"
+        value={newTask.name}
+        setValue={(name) => updateNewTask({ name })}
+        className={styles.nameInput}
+      />
+
+      {showDueDate && (
+        <InputDate
+          label="Due Date"
+          type="date"
+          value={newTask.dueDate}
+          setValue={(dueDate) => updateNewTask({ dueDate })}
+          layer={2}
+          className={styles.dueDateInput}
+        />
+      )}
+
+      {!showDueDate && (
+        <Button
+          label="Add Due Date"
+          onClick={() => setShowDueDate(true)}
+          type="secondary"
+        />
+      )}
+
+      <Button
+        label="Create"
+        onClick={handleAdd}
+        type="secondary"
+        layer={2}
+        className={styles.createButton}
+      />
+    </div>
+  );
+};
--- src/pages/tasksPage/tasksPage.tsx
+++ src/pages/tasksPage/tasksPage.tsx
@@ -1,43 +1,26 @@
 import * as React from "react";
 
-import { Button } from "components/button/button";
 import { Table } from "components/table/table";
-import { InputText } from "components/inputText/inputText";
-import { createRecord } from "services/database/createRecord";
 import { subscribeToCollection } from "services/database/subscribeToCollection";
 import { deleteRecord } from "services/database/deleteRecord";
 import { formatDate } from "utils/formatDate";
-import { mergeReducer } from "utils/mergeReducer";
 import { Task } from "types/task";
-import { DatabaseRecord } from "types/general";
-import { InputDate } from "components/inputDate/inputDate";
 
-import styles from "./styles.module.scss";
+import { TasksCreator } from "./components/tasksCreator/tasksCreator";
 
 export const TasksPage = () => {
   const [tasks, setTasks] = React.useState<Task[]>([]);
 
-  const [newTask, updateNewTask] = React.useReducer(
-    mergeReducer<Omit<Task, keyof DatabaseRecord>>,
-    {
-      dueDate: Date.now(),
-      name: "",
-    }
-  );
-
   React.useEffect(() => {
-    void subscribeToCollection({
+    const unsubcribe = subscribeToCollection<Task>({
       collection: "tasks",
-      setData: setTasks,
+      onData: setTasks,
     });
-  }, []);
 
-  const handleAdd = () => {
-    void createRecord({
-      collection: "tasks",
-      data: newTask,
-    });
-  };
+    return () => {
+      unsubcribe?.();
+    };
+  }, []);
 
   return (
     <>
@@ -71,23 +54,7 @@ export const TasksPage = () => {
         keyExtractor={(item) => item.id}
       />
 
-      <div className={styles.newTask}>
-        <InputText
-          label="Name"
-          value={newTask.name}
-          setValue={(name) => updateNewTask({ name })}
-        />
-
-        <InputDate
-          label="Due Date"
-          type="date"
-          value={newTask.dueDate}
-          setValue={(dueDate) => updateNewTask({ dueDate })}
-          layer={2}
-        />
-
-        <Button label="Create" onClick={handleAdd} type="primary" />
-      </div>
+      <TasksCreator />
     </>
   );
 };
