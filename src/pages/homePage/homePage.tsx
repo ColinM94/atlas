b808ecdcb802e946677aa1d9fa9b249f4c45f26e
Colin Maher <colinmaher@proton.me>
2025-12-19 11:19:32 +0100

    feat: add tech logos

--- src/pages/homePage/homePage.tsx
+++ src/pages/homePage/homePage.tsx
@@ -1,5 +1,14 @@
 import { Button } from 'components/button/button';
+
+import linkedinIcon from 'assets/images/linkedin.png';
+import githubIcon from 'assets/images/github.png';
+import reactIcon from 'assets/images/react.svg';
+import jsIcon from 'assets/images/js.svg';
+import tsIcon from 'assets/images/ts.svg';
+import sassIcon from 'assets/images/sass.svg';
+
 import styles from './styles.module.scss';
+import { classes } from 'utils/classes';
 
 export const HomePage = () => {
   return (
@@ -14,7 +23,44 @@ export const HomePage = () => {
         />
       </div>
       <div className={styles.content}>
-        <div className={styles.label}>colinmaher.dev</div>
+        <div className={styles.labelContainer}>
+          <div className={styles.label}>Colin Maher</div>
+          <div className={styles.subLabel}>Software Developer</div>
+        </div>
+
+        <div className={styles.section}>
+          <div className={styles.sectionLabel}>Tech</div>
+          <div className={styles.icons}>
+            <a target="_blank" href="https://react.dev">
+              <img src={reactIcon} className={styles.icon} />
+            </a>
+
+            <a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">
+              <img src={jsIcon} className={styles.icon} />
+            </a>
+
+            <a target="_blank" href="https://www.typescriptlang.org/docs/">
+              <img src={tsIcon} className={styles.icon} />
+            </a>
+
+            <a target="_blank" href="https://www.typescriptlang.org/docs/">
+              <img src={sassIcon} className={classes(styles.icon, styles.sassIcon)} />
+            </a>
+          </div>
+        </div>
+
+        <div className={styles.section}>
+          <div className={styles.sectionLabel}>Links</div>
+          <div className={styles.icons}>
+            <a target="_blank" href="https://www.linkedin.com/in/colinm94">
+              <img src={linkedinIcon} className={styles.icon} />
+            </a>
+
+            <a target="_blank" href="https://github.com/ColinM94">
+              <img src={githubIcon} className={classes(styles.icon, styles.githubIcon)} />
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
